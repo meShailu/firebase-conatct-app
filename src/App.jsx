107064d3378db2421be93776007390b1d51c78db
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-22 08:16:03 +0200

    search box is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
 
 const App = () => {
-  return <div className="bg-red-500 justify-items-stretch flex">App</div>;
+  return (
+    <div className="max-w-[370px] mx-auto px-4">
+      <Navbar />
+      <div className="flex items-center">
+        <FiSearch className="text-3xl text-white absolute ml-1" />
+        <input
+          type="text"
+          className="border-white bg-transparent h-10 rounded-md border flex-grow relative text-white pl-9"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gray: "#5A5959",
+      yellow: "#FFEAAE",
+      "dark-yellow": "#FCCA3F",
+      orange: "#F6820C",
+    },
   },
   plugins: [],
 };
