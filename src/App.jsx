94bd93fa731221f389992ee5d931b31768e77c38
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-23 07:16:37 +0200

    useffect applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { FiSearch } from "react-icons/fi";
 import { AiFillPlusCircle } from "react-icons/ai";
+import { useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function getContacts() {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactSnapShot = await getDocs(contactsRef);
+        const contactLists = contactSnapShot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactLists);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getContacts();
+  }, []);
   return (
     <div className="max-w-[370px] mx-auto px-4">
       <Navbar />
@@ -18,6 +41,7 @@ const App = () => {
 
         <AiFillPlusCircle className="text-5xl text-white cursor-pointer" />
       </div>
+      <div></div>
     </div>
   );
 };
