460277147ab13842af7b91a0367997d7185dd58e
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-24 22:23:49 +0200

    error msg implemented with help of yup for form

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Modal from "./Modal";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { toast } from "react-toastify";
+import * as Yup from "yup";
+
+const contactSchemaValidation = Yup.object().shape({
+  name: Yup.string().required("Name is Required"),
+  email: Yup.string().email("Invalid Email").required("Email is Required"),
+});
 
 const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
   const addContact = (contact) => {
@@ -31,6 +37,7 @@ const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
         <Formik
+          validationSchema={contactSchemaValidation}
           initialValues={
             isUpdate
               ? {
@@ -50,10 +57,16 @@ const AddAndUpdateContact = ({ isOpen, onClose, isUpdate, contact }) => {
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
               <Field name="name" className="h-10 border" />
+              <div className=" text-xs text-red-500">
+                <ErrorMessage name="name" />
+              </div>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email</label>
               <Field name="email" className="h-10 border" />
+              <div className=" text-xs text-red-500">
+                <ErrorMessage name="email" />
+              </div>
             </div>
             <button
               type=" button"
