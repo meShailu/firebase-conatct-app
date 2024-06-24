6ff904e51d6a5a6ed0cb1e213c165daf5805fbb9
meShailu <123243572+meShailu@users.noreply.github.com>
2024-06-24 08:51:11 +0200

    form completed with the help of formik external library

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Modal from "./Modal";
+import { Field, Form, Formik } from "formik";
 
 const AddAndUpdateContact = (isOpen, onClose) => {
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        hello
+        <Formik>
+          <Form className="flex flex-col">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <Field name="name" className="h-10 border" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field type="email" className="h-10 border" />
+            </div>
+            <button className="border bg-orange px-3 py-1.5 self-end">
+              {" "}
+              Add Contact
+            </button>
+          </Form>
+        </Formik>
       </Modal>
     </div>
   );
